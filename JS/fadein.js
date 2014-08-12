2428331562a3199eef5c216b09493e6bc46630e6
Tyler Winzenried <tyler.winzenried@byu.edu>
2014-08-11 18:08:37 -0600

    added some json stuff? some minor style modifications
    ^

--- JS/fadein.js
+++ JS/fadein.js
@@ -14,7 +14,7 @@
 		var $t						= $(this),
 			$w						= $(window),
 			viewTop			 = $w.scrollTop(),
-			botOffset		= $w.height() * .35, // offset to eliminate bottom 40% of viewport
+			botOffset		= $w.height() * 0.25, // offset to eliminate bottom 40% of viewport
 			viewBottom		= viewTop + $w.height() - botOffset, // shrinks viewport to top 60% of actual viewport
 			_top					= $t.offset().top,
 			_bottom			 = _top + $t.height(),
@@ -25,6 +25,31 @@
 	};
 })(jQuery);
 
+var $win = $(window);
+var objectsAnimate = function() {
+	console.log("objectsAnimate function is running (scroll is tripping first domino)");
+	var objects = $(".animate");
+	objects.each(function (i, el) {
+		var $el = $(el);
+		console.log("objects are getting the switch-class function attatched to them (second domino being tripped)");
+		if ($el.visible(true)) {
+			console.log("classes are being substituted third (and final domino being tripped)");
+			$(this).find(".faded-out").addClass("fade-in").removeClass("faded-out");
+			$(this).find(".comes-in").addClass("come-in");
+		}
+	});
+	
+};
+
+$win.scroll(objectsAnimate);
+
+$(document).ready(function() {
+	$(".animatable").addClass("animate").removeClass("animatable").find(".fades").addClass("faded-out");
+	$(".animate").find(".comes").addClass("comes-in");
+});
+
+
+/*
 var win = $(window);
 var doClincherThing = function() {
 	var allMods = $(".comes-in");	//	Gather all the "comes-in's"!
@@ -38,7 +63,7 @@ var doClincherThing = function() {
 };
 win.scroll(doClincherThing);
 
-/*
+
 var boxFade = function() {
 	var box = $(".fades");
 	box.each(function (i, el) {
@@ -50,7 +75,7 @@ var boxFade = function() {
 	});
 };
 
-*/
+
 
 win.scroll(boxFade);
 
@@ -64,7 +89,7 @@ $(document).ready(function() {
 	//}	
 	
 });
-
+*/
 /*
 
 
--- JS/test.js
+++ JS/test.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
 			url: "ge-courseXref.json.txt",
 			dataType: "json"
 		}).done(function( data, textStatus ) {
+			//console.log(data);
 			var geXref = data;
 			for(var key in geXref) {
 				geCourses[key] = [];
@@ -25,28 +26,59 @@ $(document).ready(function() {
 							matchRegEx = new RegExp(geXref[key]);
 						//console.log(matchRegEx);
 						if(matchRegEx.test(courseTitle)) {
-							console.log(courseTitle + " = " + key + " (" + matchRegEx.test(courseTitle) + ")");
+							//console.log(courseTitle + " = " + key + " (" + matchRegEx.test(courseTitle) + ")");
 							geData[categories[i]][j]['ge-category'] = key;
 							geCourses[key][geCourses[key].length] = geData[categories[i]][j]; 
 						} else {
 							//geData[categories[i]][j] = null;
+							//console.log(courseTitle + " = " + key + " (" + matchRegEx.test(courseTitle) + ")");
 						}
 					}
 				}
 			}
 			console.log(geCourses);
+			var geCategories = Object.keys(geCourses);
 			$(".listi").each(function(i,el) {
 				var	$el = $(el);
-				if (categories.indexOf($el.text()) > -1) {
+				if (geCategories.indexOf($el.text()) > -1) {
 					console.log($el.text()+ "? It's there!"); //it's there
-					
+					//	Process something about this...
 				}
 				else {
 					console.log($el.text()+" not found in the data provided. ("+i+")");
 					$el.css("display","none"); // or $el.remove();
 				};
 			});
+			for(var geCategory in geCourses) {
+				console.log(geCategory + ": " + geCourses[geCategory].length);
+				for(var i=0; i < geCourses[geCategory].length; i++) {
+					console.log(geCourses[geCategory][i]['title']);
+					$("#courseData").append($('<div class="majorTile">' +
+					   '    	<p class="majorTileTitle">' +
+				       '    	' + geCourses[geCategory][i]['course-title'] + '<br /> ' + geCourses[geCategory][i]['title'] + '' +
+				       '    </p>' +
+				       '    <p class="fulfilledLabel">GE Fulfilled:</p>' +
+				       '    <p class="fulfilled">' + geCategory + '</p>' +
+				       '    <p class="offered">This course is offered by BYU IS</p>' +
+				       '    <div class="buttonContainer"><a href="#"><p class="enrollButton">Enroll Now</p></a></div>' +					           
+				       '</div>'));
+				}
+			}
 		});
 	});
 });
+$(document).ready(function(){
+	$(".fallBack").remove();
+	console.log("fallBack removed");
+	$(".coursesArea").attr("style", "display: block");
+	console.log("coursesArea displayed, they have JavaScript! :D");
+});
+
+
+
+
+
+
+
+
 
