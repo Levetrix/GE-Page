ec343201959101f26d8d31daa01a1d44ef3aded8
Tyler Winzenried <tyler.winzenried@byu.edu>
2014-11-17 10:48:26 -0700

    Font changes (config.rb changes too?)

--- config.rb
+++ config.rb
@@ -1,11 +1,12 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/github/GE-Page/"
-css_dir = "stylesheets"
+http_path = "/GitHub/GE-Page/"
+css_dir = "/GitHub/GE-Page/stylesheets"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
+http_fonts_path = "/GitHub/GE-Page/fonts" 
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
